3c3f413bf5be9b7c3f4f749e85f61e9844a33674
Abhishek Gupta <abhig0209@gmail.com>
2020-10-19 14:28:48 +0530

    Warning and Error Bugs Fixed

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 matplotlib.use("Agg")
+fig , ax = plt.subplots()
+
 
 
 def main():
+    st.beta_set_page_config("Data Explorer")
     st.title("ML Dataset Explorer")
     st.subheader("Simple Data Science Explorer")
 
@@ -76,39 +79,38 @@ def main():
 
     # Plot and visualization
     st.subheader("Data Visualization")
+    all_columns_names = df.columns.tolist()
 
     ## Correlation Seaborn Plot
     if st.checkbox("Show Correlation Plot"):
-        all_columns_names = df.columns.tolist()
         st.success("Generating Correlation Plot ...")
         st.write(sns.heatmap(df.corr(),annot=True))
-        st.pyplot()
+        st.pyplot(fig)
 
     ## Count Plot
     if st.checkbox("Show Value Count Plots"):
         st.text("Value count by target")
-        all_columns_names = df.columns.tolist()
         primary_column = st.selectbox("Primary Column to Groupby",all_columns_names)
-        selected_columns_names = st.multiselect("Select Columns",all_columns_names)
+        selected_columns_names_for_cp = st.multiselect("Select Columns",all_columns_names)
         st.success("Generating Plot ...")
-        if selected_columns_names:
-            vc_plot = df.groupby(primary_column)[selected_columns_names].count()
+        if selected_columns_names_for_cp:
+            vc_plot = df.groupby(primary_column)[selected_columns_names_for_cp].count()
         else:
             vc_plot = df.iloc[:,-1].value_counts()
         st.write(vc_plot.plot(kind="bar"))
-        st.pyplot()
+        st.pyplot(fig)
 
 
     ## Pie Chart
     if st.checkbox("Show Pie Plot"):
-        all_columns_names = df.columns.tolist()
         st.success("Generating Pie Chart ...")
+        st.set_option('deprecation.showPyplotGlobalUse', False)
         st.write(df.iloc[:,-1].value_counts().plot.pie(autopct="%1.1f%%"))
-        st.pyplot()
+        st.pyplot(fig)
 
     ## Customizable Plot
     st.subheader("Customizable Plot")
-    all_columns_names = df.columns.tolist()
+    
     type_of_plot = st.selectbox("Select type of Plot",["area","bar","line","hist","box","kde"])
     selected_columns_names = st.multiselect("Select Columns to plot",all_columns_names)
 
@@ -127,9 +129,9 @@ def main():
 
         elif type_of_plot:
             custom_plot = df[selected_columns_names].plot(kind=type_of_plot)
-            st.write(custom_plot)
             st.set_option('deprecation.showPyplotGlobalUse', False)
-            st.pyplot()
+            st.write(custom_plot)
+            st.pyplot(fig)
 
     # st.balloons()
 if __name__ == "__main__":
