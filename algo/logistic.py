c52ec62ab9686529dca37b52041aa7309396245c
Abhishek Gupta <abhig0209@gmail.com>
2021-04-29 18:19:07 +0530

    project structure completed

---
+++ algo/logistic.py
@@ -0,0 +1,7 @@
+class Logistic:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ algo/naive.py
@@ -0,0 +1,7 @@
+class Naive:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
