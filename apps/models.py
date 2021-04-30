01a9e9f3e01e686675a70a7e77cc08bc13449fbf
Abhishek Gupta <abhig0209@gmail.com>
2021-04-30 14:36:01 +0530

    file restructured

--- apps/models.py
+++ apps/models.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 from .utils import select_or_upload_dataset
+from apps.algo.logistic import Logistic
+from apps.algo.knn import Knn
+from apps.algo.naive import Naive
 
 
 def modelling(df):
