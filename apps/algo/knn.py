eec740d878657c17cec1dd3f7b1440f2dfc675fb
azim2429 <abdulazim0402@gmail.com>
2021-05-01 10:32:37 +0530

    KNN Algorithm added

--- apps/algo/knn.py
+++ apps/algo/knn.py
@@ -1,7 +1,100 @@
-class Knn:
-    def __init__(self):
-        pass
+# Importing libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from scipy.stats import mode
+
+# K Nearest Neighbors Classification
 
 
-if __name__ == '__main__':
-    pass
+class K_Nearest_Neighbors_Classifier():
+
+	def __init__(self, K):
+		self.K = K
+
+	# Function to store training set
+	def fit(self, X_train, Y_train):
+		self.X_train = X_train
+		self.Y_train = Y_train
+
+		# no_of_training_examples, no_of_features
+		self.m, self.n = X_train.shape
+
+	# Function for prediction
+	def predict(self, X_test):
+		self.X_test = X_test
+
+		# no_of_test_examples, no_of_features
+		self.m_test, self.n = X_test.shape
+
+		# initialize Y_predict
+		Y_predict = np.zeros(self.m_test)
+
+		for i in range(self.m_test):
+			x = self.X_test[i]
+
+			# find the K nearest neighbors from current test example
+			neighbors = np.zeros(self.K)
+			neighbors = self.find_neighbors(x)
+
+			# most frequent class in K neighbors
+			Y_predict[i] = mode(neighbors)[0][0]
+
+		return Y_predict
+
+	# Function to find the K nearest neighbors to current test example
+	def find_neighbors(self, x):
+
+		# calculate all the euclidean distances between current
+		# test example x and training set X_train
+		euclidean_distances = np.zeros(self.m)
+
+		for i in range(self.m):
+			d = self.euclidean(x, self.X_train[i])
+			euclidean_distances[i] = d
+
+		# sort Y_train according to euclidean_distance_array and
+		# store into Y_train_sorted
+		inds = euclidean_distances.argsort()
+		Y_train_sorted = self.Y_train[inds]
+		return Y_train_sorted[:self.K]
+
+	# Function to calculate euclidean distance
+	def euclidean(self, x, x_train):
+		return np.sqrt(np.sum(np.square(x - x_train)))
+
+# Driver code
+def main():
+	pass
+	# Importing dataset
+	# df = pd.read_csv("datasets\diabetes.csv")
+	# X = df.iloc[:, :-1].values
+	# Y = df.iloc[:, -1:].values
+
+	# # Splitting dataset into train and test set
+	# X_train, X_test, Y_train, Y_test = train_test_split(
+    #         X, Y, test_size=1/3, random_state=0)
+
+	# # Model training
+	# model = K_Nearest_Neighbors_Classifier(K=3)
+	# model.fit(X_train, Y_train)
+
+	# # Prediction on test set
+	# Y_pred = model.predict(X_test)
+
+	# # measure performance
+	# correctly_classified = 0
+
+	# # counter
+	# count = 0
+
+	# for count in range(np.size(Y_pred)):
+	# 	if Y_test[count] == Y_pred[count]:
+	# 		correctly_classified = correctly_classified + 1
+	# 	count = count + 1
+
+	# print((correctly_classified / count) * 100)
+
+
+if __name__ == "__main__":
+	main()
--- apps/models.py
+++ apps/models.py
@@ -1,29 +1,101 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import preprocessing
 
 from .utils import select_or_upload_dataset
 from apps.algo.logistic import LogisticRegression
-from apps.algo.knn import Knn
+from apps.algo.knn import K_Nearest_Neighbors_Classifier
 from apps.algo.naive import Naive
 
 
 def modelling(df):
 
-    algos = ["Logistic Regression", "Naive Bayes", "K-NN"]
+    algos = ["Logistic Regression", "Naive Bayes", "K_Nearest_Neighbors_Classifier"]
     selected_algo = st.selectbox("Select Algorithm", algos)
 
     if selected_algo:
         st.info("You Selected {} Algorithm".format(selected_algo))
 
+    #Select the Taget Feature
+    column_name = df.columns
+    target_column = st.selectbox("Select Target Column", column_name)
+    target_data = df[target_column]
+
+    #Label Encoding
+    label_encoder = preprocessing.LabelEncoder()
+    encoded_target_data = label_encoder.fit_transform(target_data)
+    encoded_target_data = pd.DataFrame(encoded_target_data, columns =[target_column] )
+    st.info("You Selected {} Column".format(target_column))
+
+    # Select the Remaining Feature
+    remain_column = df.drop([target_column], axis = 1)
+    
+    #Train and Test Split
+    test_size = st.slider('Select the size of Test Dataset', min_value=0.0, max_value=1.0, value=0.3, help='Set the ratio for splitting the Train and Test Dataset')
+
+    #Set the size of K (only for K-NN)
+    if selected_algo=="K_Nearest_Neighbors_Classifier":
+        k_size = st.number_input('Select the size of K', min_value=0, max_value=100, value=0, step=1, help='Set the ratio for splitting the Train and Test Dataset')
+
+    
     if st.button("Start Training", help="Training will start for the selected algorithm on dataset"):
-        st.write("Training started...")
+        if selected_algo=="K_Nearest_Neighbors_Classifier":
+            
+            Y = encoded_target_data.values
+            X = remain_column.values
+
+            #Splitting dataset into train and test set
+            X_train, X_test, Y_train, Y_test = train_test_split(
+                    X, Y, test_size=test_size, random_state=0)
+
+            # Model training
+            if(k_size!=0):
+
+                model = K_Nearest_Neighbors_Classifier(K=k_size)
+                model.fit(X_train, Y_train)
+
+                # Prediction on test set                
+                Y_pred = model.predict(X_test)
+
+                # measure performance
+                correctly_classified = 0
+
+                # counter
+                count = 0
+
+                for count in range(np.size(Y_pred)):
+                    if Y_test[count] == Y_pred[count]:
+                        correctly_classified = correctly_classified + 1
+                    count = count + 1
+
+                st.write("Accuracy:")
+                st.info((correctly_classified / count) * 100)
+                output(Y_test, Y_pred)
+
+                # Custom Dataset Prediction 
+                # x = np.array([[5.8000 , 2.8000,5.1000,2.4000]])
+                # dataClass = model.predict(x)
+                # st.write(dataClass)
+
+#Displays the performance of the model
+def output(Y_test,Y_pred):
+    confusion_matrix_out = confusion_matrix(Y_test, Y_pred)
+    st.write("Confusion Matrix:")
+    st.write(confusion_matrix_out)
+
+    classification_report_out = classification_report(Y_test, Y_pred)
+    st.write("Classification Report:")
+    st.write(classification_report_out)
 
 
 def app():
     st.title("Machine Learning Modelling")
     st.subheader("Choose your dataset ")
-
     select_or_upload_dataset(modelling)
-
+    
 
 if __name__ == "__main__":
     app()
